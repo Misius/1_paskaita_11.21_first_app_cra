8cfe4b294a7a533406afc1ea069d8e541ef33300
Robertas Misevičius <robertas31810@gmail.com>
2022-11-21 14:51:26 +0200

    cards

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
 import './App.css';
 
+import house5 from './house5.jpg';
+
+const card = <h3>Hello</h3>
+
+const card1 = (
+  <div className='card'>
+        <h4 className='cardTitle'>House 2</h4>
+        <img className='cardImg' src='/img/house2.jpg' alt="antras namas" />
+      </div>
+);
+
+const card2 = (
+  <div className='card'>
+        <h4 className='cardTitle'>House 5</h4>
+        <img className='cardImg' src={house5} alt="penktas namas" />
+      </div>
+);
+
 function App() {
+  const name = 'James';
   return (
     <div className="App">
-      <h2 className="mainTitle">Hello React</h2>
+      <h1 className="">Hello {name} {150 * 30}</h1>
+      <h2>{card}</h2>
+      {card2}
+      <hr />
+      {card1}
+      
     </div>
   );
 }
